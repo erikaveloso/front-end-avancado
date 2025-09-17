58e076a4d363ea85abbb10473ca860fa6913be33
Erika Veloso <erikaveloso@msn.com>
2025-09-17 20:56:10 -0300

    dia 17/09

--- aula03/src/App.jsx
+++ aula03/src/App.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
 import EstouConseguindoAprenderReact from './components/EstouConseguindoAprenderReact'
 import EstouComSon from './components/EstouComSono'
+import Aluno from './components/Aluno'
+import Linguagens from './components/Linguagens'
+import Produto from './components/Produto'
 
 const App = () => {
   return (
     <div>
       <EstouConseguindoAprenderReact estouConseguindo={false} />
       <EstouComSon comSono={true} />
+
+      {
+        [
+          {nome: "Érika", email: "erikaveloso@msn.com", curso: "Sistemas para Internet", media: 10.0 },
+          {nome: "Luiz Eduardo", email: "luizeduardo@gmail.com", curso: "Sistemas para Internet", media: 8.0 },
+          {nome: "Richelle", email: "richelle@hotmail.com", curso: "Sistemas para Internet", media: 6.0 }
+        ].map((aluno) => 
+          <Aluno nome={aluno.nome} email={aluno.email} curso={aluno.curso} media={aluno.media} />
+        )
+      }
+
+      {
+        [
+          {nome: "Java", dificuldade: "fácil" },
+          {nome: "Python", dificuldade: "médio"},
+          {nome: "JavaScript", dificuldade: "difícil"}
+        ].map((linguagens) => 
+          <Linguagens nome={linguagens.nome} dificuldade={linguagens.dificuldade} />
+        )
+      }
+
+
+      {
+        [
+          {nome: "Garrafa", preco: 50, categoria: "utilidades", quantidade: 0 },
+          {nome: "Toalha", preco: 70, categoria: "utilidades", quantidade: 2},
+          {nome: "Mochila", preco: 250, categoria: "utilidades", quantidade: 10}
+        ].map((produto) => 
+          <Produto nome={produto.nome} preco={produto.preco} categoria={produto.categoria} quantidade={produto.quantidade}/>
+        )
+      }
+
     </div>
   )
 }
---
+++ aula03/src/components/Linguagens.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Linguagens = ({nome, dificuldade}) => {
+  return (
+    <div>
+        <p>Nome: {nome}</p>
+        <p>Dificuldade: {dificuldade}</p>
+    </div>
+  )
+}
+
+export default Linguagens
