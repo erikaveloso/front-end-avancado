3aacc8b70866708708edefa6710d763a8e796531
Erika Veloso <erikaveloso@msn.com>
2025-09-24 21:08:32 -0300

    dia 24/09

---
+++ aula04/src/components/Exercicio1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exercicio1 = () => {
+  return (
+    <div>
+        <img src="imagempassaro.jpg" alt="passaro" />
+        Foto pássaro
+    </div>
+  )
+}
+
+export default Exercicio1
---
+++ aula04/src/components/Exercicio2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Exercicio2 = () => {
+  return (
+    <div>
+        <div style={{color: '#ff0000', backgroundColor: '#0000ff', width: '200px', textAlign: 'center' }}>Estou aprendendo CSS no React!</div>
+    </div>
+  )
+}
+
+export default Exercicio2
