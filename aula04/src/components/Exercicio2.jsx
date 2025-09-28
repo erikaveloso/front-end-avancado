2151d4e797a0f6fcc1d2096ac9fdf1a19c6765e1
Erika Veloso <erikaveloso@msn.com>
2025-09-28 19:13:40 -0300

    correção dia 24/09

--- aula04/src/components/Exercicio2.jsx
+++ aula04/src/components/Exercicio2.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Exercicio2 = () => {
   return (
-    <div>
-        <div style={{color: '#ff0000', backgroundColor: '#0000ff', width: '200px', textAlign: 'center' }}>Estou aprendendo CSS no React!</div>
-    </div>
+        <div style={{color: '#ff0000', backgroundColor: '#0000ff', width: '200px', textAlign: 'center' }}>Estou aprendendo CSS no React!
+        <p style={{fontSize: '100px'}}> Teste</p>
+        </div>
   )
 }
 
