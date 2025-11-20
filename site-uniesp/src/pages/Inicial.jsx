9519ed63ef594fd3cad36b744bfd5656fc1822ca
Erika Veloso <erikaveloso@msn.com>
2025-11-19 21:02:35 -0300

    dia 19/11

---
+++ site-uniesp/src/pages/Inicial.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { Card, Container, Row, Col } from "react-bootstrap";
+
+const Inicial = () => {
+  return (
+    <Container className="py-4">
+      <Card className="mb-4 shadow-sm">
+        <p> A Faculdade UNIESP é uma instituição comprometida em fornecer educação de qualidade, com foco no desenvolvimento profissional e pessoal... </p>
+        <p> Contamos com uma equipe de professores qualificados, uma infraestrutura moderna e uma metodologia de ensino que valoriza o aprendizado prático... </p>
+        <p> A seguir, conheça mais sobre as áreas e as oportunidades que a Faculdade UNIESP oferece para você. </p>
+      </Card>
+
+      <Row className="g-4">
+        <Col md={4}>
+        <Card className="text-center h-100 shadow-sm">
+            <Card.Img
+                variant="top" 
+                src="https://images.unsplash.com/photo-1556761175-4b46a572b786"
+                alt="Main Building"
+                style={{ height: '200px', objectFit: 'cover' }}
+            />
+            <Card.Body>
+                <Card.Title>
+                    Vida no Campus
+                </Card.Title>
+                <Card.Text>
+                  Um ambiente vibrante e acolhedor
+                </Card.Text>
+
+            </Card.Body>
+            
+        </Card>
+        </Col>
+
+        <Col md={4}>
+        <Card className="text-center h-100 shadow-sm">
+            <Card.Img
+                variant="top" 
+                src="https://images.unsplash.com/photo-1571260899304-425eee4c7efc"
+                alt="Main Building"
+                style={{ height: '200px', objectFit: 'cover' }}
+            />
+            <Card.Body>
+                <Card.Title>
+                    Biblioteca
+                </Card.Title>
+                <Card.Text>
+                  Uma biblioteca com um acervo completo e atualizado
+                </Card.Text>
+
+            </Card.Body>
+            
+        </Card>
+        </Col>
+
+        <Col md={4}>
+        <Card className="text-center h-100 shadow-sm">
+            <Card.Img
+                variant="top" 
+                src="https://images.unsplash.com/photo-1652974731232-efc86a9bd985"
+                alt="Main Building"
+                style={{ height: '200px', objectFit: 'cover' }}
+            />
+            <Card.Body>
+                <Card.Title>
+                    Prédio Principal
+                </Card.Title>
+                <Card.Text>
+                  O centro da nossa instituição, com salas de aula modernas e laboratórios equipados para atender às necessidades dos cursos.
+                </Card.Text>
+
+            </Card.Body>
+            
+        </Card>
+        </Col>
+
+      </Row>
+
+      <Row className="g-4 mt-4">
+        <Col md={6}>
+        <Card className="h-100 shadow-sm">
+          <Card.Img
+          variant="top"
+          src="https://images.unsplash.com/photo-1521790797524-b2497295b8a0"
+          style={{ height: '200px', objectFit: 'cover' }}
+
+          />
+
+          <Card.Body className="text-center">
+            <Card.Title>Parceria com empresas</Card.Title>
+            <Card.Text>
+              Mantemos acordos com empresas locais e multinacionais
+            </Card.Text>
+
+          </Card.Body>
+
+        </Card>
+        </Col>
+
+        <Col md={6}>
+        <Card className="h-100 shadow-sm">
+          <Card.Img
+          variant="top"
+          src="https://images.unsplash.com/photo-1513258496099-48168024aec0"
+          style={{ height: '200px', objectFit: 'cover' }}
+
+          />
+
+          <Card.Body className="text-center">
+            <Card.Title>Compromisso com a educação</Card.Title>
+            <Card.Text>
+              Investimos com a educação de qualiadade 
+            </Card.Text>
+
+          </Card.Body>
+
+        </Card>
+        </Col>
+
+        <Col md={6}>
+        </Col>
+      </Row>
+
+      
+    </Container>
+  );
+};
+
+export default Inicial;
